2b6aee587f5deff8750519ea54b80c8c58d8a1ac
kravchuks <43855399+kravchuks@users.noreply.github.com>
2021-11-05 21:52:32 +0200

    test

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.inner__goods-slider').lightSlider({
+      adaptiveHeight:true,
+      item:1,
+      slideMargin:50,
+      loop:true,
+      
+    });
+
+    $('.reviews__inner').lightSlider({
+      adaptiveHeight:true,
+      item:3,
+      slideMargin:50,
+      loop:true,
+    })
+    
+  });
